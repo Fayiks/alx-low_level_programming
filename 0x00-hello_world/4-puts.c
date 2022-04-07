7a432a463e7626b7cc159cf067bef7e93f0c4c74
Your Name <osatfay27@gmail.com>
2022-04-07 14:39:29 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
  *
- * Return : Always 0 (Sucess)
- */
+ * *main - function begins
+ *
+ * *
+ *
+ * * Return: function ends
+ *
+ * */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-	
+		return (0);
 }
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
 
 
