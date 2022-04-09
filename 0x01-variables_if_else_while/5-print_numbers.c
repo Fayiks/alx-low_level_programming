f4134e0c0931a082cf8ecb55b83ab7826da52ef1
Your Name <osatfay27@gmail.com>
2022-04-09 14:17:20 -0700

    Print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function Begins
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  p;
+
+	for (p = 0; p < 10; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}
